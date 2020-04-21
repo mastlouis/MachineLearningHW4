b5565a501aca8f13a491b8d2ada12bbdfcc9d664
Linda Puzey <47217463+lpuzey@users.noreply.github.com>
2020-04-21 14:24:51 -0400

    Half of Part 2 DONE

--- homework4_santander_template.py
+++ homework4_santander_template.py
@@ -1,29 +1,60 @@
-import sklearn.svm
-import sklearn.metrics
+from sklearn.svm import LinearSVC
+from sklearn.metrics import roc_auc_score
 import numpy as np
 import pandas
 
+# Linear SVM
+
 # Load data
+# train.csv contains 200,000 entries
 d = pandas.read_csv('train.csv')
 y = np.array(d.target)  # Labels
-X = np.array(d.iloc[:,2:])  # Features
+X = np.array(d.iloc[:, 2:])  # Features
+
+#half the number of all the examples
+half = 100000
 
 # Split into train/test folds
-# TODO
 
-# Linear SVM
-# TODO
+# Ensures that the randoms remain the same everytime
+np.random.seed(0)
+data = np.arange(X.shape[0])
 
-# Non-linear SVM (polynomial kernel)
-# TODO
+# Shuffles the ordering of the data
+np.random.shuffle(data)
+
+# Creating split data sets
+y = y[data]
+X = X[data]
+
+# Creating training and testing sets
+xtr = X[:half, :]
+xte = X[half:, :]
+
+ytr = y[:half, ]
+yte = y[half:, ]
 
 # Apply the SVMs to the test set
-#yhat1 = ...  # Linear kernel
-#yhat2 = ...  # Non-linear kernel
+classifier = LinearSVC(dual=False)
+classifier.fit(xtr, ytr)
+
+# Predictions
+yhat1 = classifier.decision_function(xte) # Linear kernel
 
 # Compute AUC
-#auc1 = ...
-#auc2 = ...
+auc1 = roc_auc_score(yte, yhat1)
+print('Linear Kernel SVM Accuracy:', auc1)
 
-print(auc1)
-print(auc2)
+
+
+# Non-linear SVM (polynomial kernel)
+
+
+# Apply the SVMs to the test set
+# yhat2 = ...  # Non-linear kernel
+
+# Compute AUC
+# auc2 = ...
+
+
+
